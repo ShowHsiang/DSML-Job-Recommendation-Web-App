c9b2127b8f6fbb5c6a6211c3c1f046b04eccd61d
Shawn Hsiang <133613082+ShowHsiang@users.noreply.github.com>
2023-12-24 16:01:32 +0800

    Add files via upload

---
+++ Main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.title("Main Page")
+
+st.write("""
+🚀 Welcome to the DSML Insights Hub! 🚀
+
+Embark on a data-driven journey into the fascinating world of Data Science and Machine Learning (DSML). Our platform is designed to unravel insights and trends from Kaggle's Annual Machine Learning and Data Science Surveys spanning 2020 to 2022.
+
+📈 Explore the Landscape of DSML: Your Data Odyssey Begins
+
+At DSML Insights Hub, we're thrilled to guide you through Exploratory Data Analysis (EDA), providing a panoramic view of the evolving DSML industry. Our user-friendly Streamlit app introduces you to engaging functionalities, allowing you to dive into the data and make meaningful discoveries:
+
+Trend Over Time:
+
+Choose a key factor from our curated list of features, each offering unique insights.
+Use our filter to select a specific scope to reveal dynamic trends from 2020-2022.
+
+Statistical Tendencies:
+
+Customize your analysis by selecting a feature and a time period of interest.
+Explore statistical measures and insightful scopes to uncover the intricacies of DSML statistics.
+
+🔍 Job Recommendation Engine:
+
+Dive into our designed job recommendation app, built on insights from the Kaggle dataset.
+Simply input your professional details, such as skills, experience, and preferences.
+Receive tailored job recommendations that align with your profile.
+This feature aims to connect you with suitable DSML opportunities, paving your way to a fulfilling career.
+Our goal is to make data exploration accessible, insightful, and practical. Engage with the data, discover trends, and deepen your understanding of the DSML sector. The insights you gather here will not only broaden your knowledge but will also assist you in navigating your career path with our job recommendation tool.
+
+📊 Ready to uncover the stories hidden in the data and explore career opportunities tailored for you? Click away and let the data unfold the narrative of DSML! 🌐✨
+""")
